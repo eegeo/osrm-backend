a8ab4d5b9523b61486eb2fbd1691725fb7f33bf0
jlunderwood <judith.underwood@eegeo.com>
2016-08-03 14:44:35 +0100

    Adjust magic numbers to permit more alternative routes
    Buddy: Ian H

--- include/engine/routing_algorithms/alternative_path.hpp
+++ include/engine/routing_algorithms/alternative_path.hpp
@@ -21,9 +21,9 @@ namespace engine
 namespace routing_algorithms
 {
 
-const double VIAPATH_ALPHA = 0.10;
-const double VIAPATH_EPSILON = 0.15; // alternative at most 15% longer
-const double VIAPATH_GAMMA = 0.75;   // alternative shares at most 75% with the shortest.
+const double VIAPATH_ALPHA = 5.0;   // alternative can add significant nonoverlapping distance
+const double VIAPATH_EPSILON = 1.00; // alternative at most twice as long
+const double VIAPATH_GAMMA = 0.85;   // alternative shares at most 85% with the shortest.
 
 template <class DataFacadeT>
 class AlternativeRouting final
